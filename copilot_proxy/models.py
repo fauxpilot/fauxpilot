8895b742384181ad8abc6234029f5f95bfedbcc7
Fred de Gier <fdegier@gitlab.com>
2022-09-12 12:59:37 +0200

    Rewrite API to FastAPI, separate API from CodeGen, remove dev settings

---
+++ copilot_proxy/models.py
@@ -0,0 +1,23 @@
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class OpenAIinput(BaseModel):
+    model: str
+    prompt: Optional[str]
+    suffix: Optional[str]
+    max_tokens: Optional[int] = 16
+    temperature: Optional[float] = 0.6
+    top_p: Optional[float] = 1.0
+    n: Optional[int] = 1
+    stream: Optional[bool]
+    logprobs: Optional[int] = None
+    echo: Optional[bool]
+    stop: Optional[Union[str, list]]
+    presence_penalty: Optional[float] = 0
+    frequency_penalty: Optional[float] = 1
+    best_of: Optional[int] = 1
+    logit_bias: Optional[dict]
+    user: Optional[str]
+
