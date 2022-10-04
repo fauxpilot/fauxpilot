322016851e0cb3d9c6a890ef3f7b5e51de42d6cb
Jingbei Li <i@jingbei.li>
2022-10-04 11:45:08 +0800

    made model optional for copilot proxy

--- copilot_proxy/models.py
+++ copilot_proxy/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 
 class OpenAIinput(BaseModel):
-    model: str
+    model: Optional[str] = 'codegen'
     prompt: Optional[str]
     suffix: Optional[str]
     max_tokens: Optional[int] = 16
