f1ab866117ba1a7de9986f4917aeed322f9c816b
Geunsik Lim <geunsik.lim@samsung.com>
2022-10-18 13:33:13 +0900

    Added pop-up window to generate help message
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

---
+++ client/help.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta content="IE=edge" http-equiv="X-UA-Compatible">
+<meta content="width=1200" name="viewport">
+<link href="./style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body bgcolor=#D4E6F1>
+<h2>Example: Source Code</h2>
+<br>
+<hr>
+/* Get the largest prime number that smaller than input */<br>
+int GetPrimeNumber(int input)<br>
+<hr>
+# Get the largest prime number that smaller than input<br>
+def GetPrimeNumber(input):
+<hr>
+// Get the largest prime number that smaller than input<br>
+public static boolean isPrime(int n)<br>
+<hr>
+import static org.junit.Assert.*;<br>
+import org.junit.Test;<br>
+public class IsPrimeTest {<br>
+  // Math.isPrime(int) returns whether the given number is prime or not<br>
+  @Test<br>
+<hr>
+Create a SQL request to find all users who live in South Korea and have over 1000 credits:<br>
+<hr>
+<br>
+</body>
+</html>
