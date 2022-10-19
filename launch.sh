986c342cad0ab9b36363e34f08967482a8064c51
Fred de Gier <freddegier@me.com>
2022-10-19 14:52:22 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -8,8 +8,4 @@ fi
 source .env
 
 # On newer versions, docker-compose is docker compose
-if command -v docker-compose > /dev/null; then
-    docker compose up
-else
-    docker-compose up
-fi
+docker compose up -d --remove-orphans || docker-compose up -d --remove-orphans
